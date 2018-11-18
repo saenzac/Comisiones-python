976eb38ff9a738072be2b211ae39e9ebf0e09bcd
Johnny Saenz <saenzac@gmail.com>
2018-11-18 15:48:14 -0500

    updated with new path handling method

---
+++ Loader/globals.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 13 16:00:01 2017
+
+@author: scarebyte
+
+"""
+
+configini = 'Z:/Developer/Comisiones4/Config/myconfig.ini'
+dbconfigini = 'Z:/Developer/Comisiones4/Config/mydbconfig.ini'
